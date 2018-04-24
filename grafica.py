48862b773073dcd98fc4a1484de35b2564580f3c
Diego Garzon <da.garzon1@uniandes.edu.co>
2018-04-24 07:52:29 -0500

    difusion

--- grafica.py
+++ grafica.py
@@ -4,6 +4,6 @@ import matplotlib.pyplot as plt
 data=np.loadtxt("datos.txt")
 label=data[0,3];
 plt.plot(data[:,0], data[:,2], label="initial", color='b')
-plt.plot(data[:,0], data[:,1], label="t=%s"%label, color='r')
+plt.plot(data[:,0], data[:,1], label="t=% s"%label, color='r')
 plt.legend()
 plt.savefig("grafica.pdf")
